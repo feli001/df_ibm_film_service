16b8f4b52b1723d9da969c2c1936bec7eecb8c05
Daniel Feledi <feledi.daniel@gmail.com>
2016-10-26 12:40:45 +0200

    Initial Commit

---
+++ DF_Film_Service/DF_IBM_SF_Film_Service/src/film_service/DfFilmServlet.java
@@ -0,0 +1,33 @@
+package film_service;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DfFilmServlet
+ */
+@WebServlet("/DfFilmServlet")
+public class DfFilmServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DfFilmServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+
+}
---
+++ DF_Film_Service/DF_IBM_SF_Film_Service/src/film_service/DfFilmServletTest.java
@@ -0,0 +1,15 @@
+package film_service;
+
+import junit.framework.TestCase;
+
+public class DfFilmServletTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+}
