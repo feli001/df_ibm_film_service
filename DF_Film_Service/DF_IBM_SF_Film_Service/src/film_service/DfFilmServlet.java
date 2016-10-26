99b35e2e616d7161f8d6aabd101615b71d970744
Daniel Feledi <feledi.daniel@gmail.com>
2016-10-26 14:30:15 +0200

    Finished: - Created Test for data filtering - Reading data from specified URL - Filtering data - Returning filtered data as response

--- DF_Film_Service/DF_IBM_SF_Film_Service/src/film_service/DfFilmServlet.java
+++ DF_Film_Service/DF_IBM_SF_Film_Service/src/film_service/DfFilmServlet.java
@@ -1,33 +1,120 @@
 package film_service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Iterator;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.*;
+
 /**
  * Servlet implementation class DfFilmServlet
  */
 @WebServlet("/DfFilmServlet")
 public class DfFilmServlet extends HttpServlet {
+	
+	private static final String SF_FILM_DB_URL = "https://data.sfgov.org/resource/wwmu-gmzc.json";
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DfFilmServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
 
 	/**
+	 * Reads from a prespecified url, parses and filters the content and responds with a JSON compatible string 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		URL url = new URL(SF_FILM_DB_URL);
+		
+		String readString = readStringFromURL(url);
+		String outputString = new String();
+		try{
+			JSONArray jsonInput = new JSONArray(readString);
+			String[] properties = {"locations","title"};
+			JSONArray jsonOutput = reduceProperties(jsonInput, properties);
+			outputString = jsonOutput.toString();
+			
+		}catch(JSONException e){
+			System.err.println("Error parsing JSONArray: " + e.getMessage());
+			outputString = "There was an error reading from the JSON source";
+		}
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print(outputString);
+		out.flush();
+		out.close();
+		
+		
 	}
 
+	/**
+	 * Reads a string from an URL and returns the string
+	 * @param url URL from which to read
+	 * @return String read from the URL
+	 */
+	private String readStringFromURL(URL url){
+		BufferedReader reader = null;
+		try{
+			reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			StringBuffer buffer = new StringBuffer();
+			int read;
+			char[] chars = new char[2048];
+			while((read = reader.read(chars)) != -1){
+				buffer.append(chars, 0, read);
+			}
+			return buffer.toString();
+		} catch (IOException e) {
+			System.err.println("IOException while trying to read from url: " + e.getMessage());
+			return new String();
+		}finally{
+			if(reader != null){
+				try {
+					reader.close();
+				} catch (IOException e) {
+					System.err.println("Could not close reader: " + e.getMessage());
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Reduces the JSONArray to contain only the desired properties
+	 * @param inputArray A JSONArray
+	 * @param properties A String Array listing all the desired properties, which should not be removed from the array.
+	 * @return Returns a JSONArray containing only the required properties
+	 */
+	public JSONArray reduceProperties(JSONArray inputArray, String... properties ){
+		JSONArray result = new JSONArray();
+		if(inputArray != null){
+			for(int i = 0; i < inputArray.length(); i++){
+				JSONObject obj;
+				try {
+					obj = inputArray.getJSONObject(i);
+					Iterator keyIterator = obj.keys();
+					while(keyIterator.hasNext()){
+						String key = (String)keyIterator.next();
+						boolean toDelete = true;
+						if(!Arrays.asList(properties).contains(key)){
+							keyIterator.remove();
+						}
+						
+					}
+					result.put(obj);
+				} catch (JSONException e) {
+					System.err.println("JSON input was not correct. Error: " + e.getMessage());
+				}
+				
+			}
+		}
+		
+		return result;
+	}
 
 }
--- DF_Film_Service/DF_IBM_SF_Film_Service/src/film_service/DfFilmServletTest.java
+++ DF_Film_Service/DF_IBM_SF_Film_Service/src/film_service/DfFilmServletTest.java
@@ -1,6 +1,14 @@
 package film_service;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import junit.framework.TestCase;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import org.json.*;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 public class DfFilmServletTest extends TestCase {
 
@@ -8,8 +16,28 @@ public class DfFilmServletTest extends TestCase {
 		super.setUp();
 	}
 
-	public void testDoGetHttpServletRequestHttpServletResponse() {
+	public void testServlet() throws Exception{
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpServletResponse response = mock(HttpServletResponse.class);
 		fail("Not yet implemented");
+		
+		
+	}
+	
+	public void testJsonFilter() throws JSONException{
+		JSONObject testObject = new JSONObject("{\"actor_1\":\"Siddarth\",\"actor_2\":\"Nithya Menon\",\"actor_3\":\"Priya Anand\",\"director\":\"Jayendra\",\"locations\":\"Epic Roasthouse (399 Embarcadero)\",\"production_company\":\"SPI Cinemas\",\"release_year\":\"2011\",\"title\":\"180\",\"writer\":\"Umarji Anuradha, Jayendra, Aarthi Sriram, & Suba \"}");
+		JSONArray input = new JSONArray();
+		input.put(testObject);
+		
+		DfFilmServlet servlet = new DfFilmServlet();
+		String[] properties = {"locations","title"};
+		JSONArray result = servlet.reduceProperties(input, properties);
+		
+		JSONObject expectedObject = new JSONObject("{\"locations\":\"Epic Roasthouse (399 Embarcadero)\",\"title\":\"180\"}");
+		JSONArray expected = new JSONArray();
+		expected.put(expectedObject);
+		
+		JSONAssert.assertEquals(expected, result, true);
 	}
 
 }
